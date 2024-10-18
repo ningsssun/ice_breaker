c4a758fc2d5982928234fe9a8afb0089b2b248c4
sunning <ning.sssun@gmail.com>
2024-10-18 17:52:20 +0200

    fix variable issue

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -42,6 +42,6 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
 if __name__ == "__main__":
     print(
         scrape_linkedin_profile(
-            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/",
+            linkedin_profile_url="https://www.linkedin.com/in/ning-sun-3206b819b/",
         )
     )
